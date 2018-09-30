a41aa1f1814733fe47bbb1884ca678ac7eb09889
Chinnatip Taemkaeo <chinnatip@me.com>
2018-09-30 18:16:47 +0700

    manga catalog

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import 'antd/dist/antd.css'
-import logo from './logo.svg'
-import './App.css'
-import Sample from './component/Sample'
-import { DatePicker } from 'antd'
+import './style/App.css'
+import './style/Antd.css'
+import Brand from './component/Brand'
+import { Layout } from 'antd'
+
+function importAll(r) {
+  return r.keys().map(r)
+}
+
+const imagesImport = importAll(
+  require.context('./assets/manga/', false, /\.(png|jpe?g|svg)$/)
+)
+
+const { Header, Footer, Sider, Content } = Layout
+
+const imageAmount = imagesImport.length + 1
+const imageWidth = 540
+
+const Canvas = styled.div`
+  flex-grow: 1;
+  overflow-x: scroll;
+  &::-webkit-scrollbar {
+    height: 4px;
+  }
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+  }
+  div._container {
+    height: 100%;
+    width: ${imageAmount * imageWidth}px;
+    span._lefter {
+      width: 500px;
+      display: inline-block;
+    }
+    img {
+      height: 98%;
+      opacity: 0.35;
+      margin-right: 4px;
+      cursor: pointer;
+      transition: 0.35s;
+      &._active {
+        opacity: 1;
+        box-shadow: 0 0 5px grey;
+      }
+    }
+  }
+`
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: 0
+    }
+    this.handleActive = this.handleActive.bind(this)
+  }
+  componentDidMount() {}
+  handleActive(pointer) {
+    this.setState({
+      active: pointer
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Sample />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <Header>
+            <Brand />
+          </Header>
+          <Layout>
+            {/* <Sider>
+              <Sample />
+            </Sider> */}
+            <Content>
+              <Canvas>
+                <div className="_container">
+                  <span className="_lefter" />
+                  {imagesImport.map((img, index) => (
+                    <img
+                      className={this.state.active === index && '_active'}
+                      onMouseOver={() => this.handleActive(index)}
+                      src={img}
+                      alt=""
+                    />
+                  ))}
+                </div>
+              </Canvas>
+            </Content>
+          </Layout>
+        </Layout>
       </div>
     )
   }
---
+++ src/component/Brand.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.svg'
+
+const BrandHeader = styled.span`
+  img {
+    height: 32px;
+    vertical-align: text-bottom;
+  }
+  h1 {
+    display: inline-block;
+    vertical-align: super;
+  }
+`
+
+class Brand extends Component {
+  render() {
+    return (
+      <BrandHeader>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Mangarizer</h1>
+      </BrandHeader>
+    )
+  }
+}
+
+export default Brand
